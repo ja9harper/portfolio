b98f53d484e06ac88b75a23cbe05eeaeca14f919
Janine Harper <jaharper@750-MAC-MPG8WL.local>
2019-04-03 13:48:44 -0400

    changed debug to false and set allowed hosts

--- jahportfolio/settings.py
+++ jahportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cx57p464d!kvjmkna@#3d!&#(&hn^opuwu882$y2s#-438j$5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
